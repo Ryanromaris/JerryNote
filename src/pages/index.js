f607a6a2089f0fd8df607f9d2113422e39e5179a
ryanromaris <ryanromaris@naver.com>
2022-05-22 20:34:32 +0900

    feature(index): full page scroll in LandingPage, 자기소개 내용 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../styles/LandingPage.module.css";
+import { FullPage, Slide } from "react-full-page";
+import Page1 from "../components/LandingPage/page1";
 
 const LandingPage = () => {
   return (
-    <div>
-      <Link
-        style={{ color: "inherit", textDecoration: "inherit" }}
-        to="/timerGameMain"
-      >
-        <div>Go to Jerry Game Town!</div>
-      </Link>
-    </div>
+    <>
+      <FullPage>
+        <Slide>
+          <Page1 />
+        </Slide>
+        <Slide>
+          <h1>Another slide content</h1>
+        </Slide>
+        <Slide>
+          <h1>Another slide content</h1>
+        </Slide>
+        <Slide>
+          <h1>Another slide content</h1>
+        </Slide>
+        <Slide>
+          <h1>Another slide content</h1>
+        </Slide>
+        <Slide>
+          <h1>Another slide content</h1>
+        </Slide>
+      </FullPage>
+
+      <div>
+        <Link
+          style={{ color: "inherit", textDecoration: "inherit" }}
+          to="/timerGameMain"
+        >
+          <div>Go to Jerry Game Town!</div>
+        </Link>
+      </div>
+    </>
   );
 };
 
