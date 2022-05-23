8c800e49785f267a67398f40ceb4a2ac8dce5f2d
ryanromaris <ryanromaris@naver.com>
2022-05-23 23:56:17 +0900

    feature(page2): self photo 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import styles from "../styles/LandingPage.module.css";
 import { FullPage, Slide } from "react-full-page";
 import Page1 from "../components/LandingPage/page1";
+import Page2 from "../components/LandingPage/page2";
 
 const LandingPage = () => {
   return (
@@ -12,10 +13,17 @@ const LandingPage = () => {
           <Page1 />
         </Slide>
         <Slide>
-          <h1>Another slide content</h1>
+          <Page2 />
         </Slide>
         <Slide>
-          <h1>Another slide content</h1>
+          <div>
+            <Link
+              style={{ color: "inherit", textDecoration: "inherit" }}
+              to="/timerGameMain"
+            >
+              <div>Go to Jerry Game Town!</div>
+            </Link>
+          </div>
         </Slide>
         <Slide>
           <h1>Another slide content</h1>
@@ -27,15 +35,6 @@ const LandingPage = () => {
           <h1>Another slide content</h1>
         </Slide>
       </FullPage>
-
-      <div>
-        <Link
-          style={{ color: "inherit", textDecoration: "inherit" }}
-          to="/timerGameMain"
-        >
-          <div>Go to Jerry Game Town!</div>
-        </Link>
-      </div>
     </>
   );
 };
